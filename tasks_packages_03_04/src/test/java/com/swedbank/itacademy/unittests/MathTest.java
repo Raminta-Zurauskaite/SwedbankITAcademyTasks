6058d3f1deb204c31de83e94ca786ddb158dd7a8
Raminta <ramintules@gmail.com>
2022-03-14 15:25:34 +0200

    Test and gitignore updates

--- tasks_packages_03_04/src/test/java/com/swedbank/itacademy/unittests/MathTest.java
+++ tasks_packages_03_04/src/test/java/com/swedbank/itacademy/unittests/MathTest.java
@@ -1,6 +1,9 @@
 package com.swedbank.itacademy.unittests;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class MathTest {
     @BeforeAll
